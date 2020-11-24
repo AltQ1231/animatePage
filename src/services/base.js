2df05398382adb5da19400a1194e0e5fdd74d1b7
zhong.li_HQ-CDIC <zhong.li@united-imaging.com>
2020-11-24 14:00:53 +0800

    delete

--- src/services/base.js
+++ src/services/base.js
@@ -7,27 +7,3 @@ export async function getCheckPackage() {
   // console.log('start request')
   return request('/checkPackage');
 }
-
-
-// 生产效率
-export async function getEfficiencyData(params) {
-  return request('/efficiency/data', { params });
-}
-
-// 生产进度
-export async function getScheduleOverviewData(params) {
-  return request('/progress/overview', { params });
-}
-
-export async function getScheduleChartsData(params) {
-  return request('/progress/charts', { params });
-}
-
-export async function getScheduleDetailData(params) {
-  return request('/progress/detail', { params });
-}
-
-// 损失工时
-export async function getLostTimeData(params) {
-  return request('/hour/loss/data', { params });
-}
